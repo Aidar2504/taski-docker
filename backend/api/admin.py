8aefe619cff4858389f71ad774097a074b170484
Aidar2504 <aidar25041995@gmail.com>
2025-07-05 01:37:47 +0500

    Fix import blank lines and add docstrings

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,12 @@
-from django.contrib import admin
+"""Регистрация моделей в административной панели Django."""
 
+from django.contrib import admin
 from .models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
+    """Регистрация моделей в административной панели Django."""
+
     list_display = ('title', 'description', 'completed')
 
 
